1dcb77c65e817936fc64278969a3238b414677f9
mechanicalhuman <social@mechanicalhuman.com>
2013-10-10 15:46:03 -0400

    Syphonpart2

--- NWS_GenericSyphon_OSC_IO/src/main.cpp
+++ NWS_GenericSyphon_OSC_IO/src/main.cpp
@@ -1,6 +1,6 @@
 #include "ofMain.h"
 #include "testApp.h"
-
+#include "ofAppGlutWindow.h"
 //========================================================================
 int main( ){
 	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
--- NWS_GenericSyphon_OSC_IO/src/testApp.cpp
+++ NWS_GenericSyphon_OSC_IO/src/testApp.cpp
@@ -2,16 +2,54 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
+    
+    // Syphon init
+    ofSetWindowTitle("Generic IO");
+    mainOutput.setName("GenericIO_Output");
+    
+    //render reset
+    ofBackground(0);
+    ofSetVerticalSync( TRUE );
+    ofEnableAlphaBlending();
+    ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+    ofSetBackgroundAuto(TRUE);
+    ofSetFrameRate(60);
 
+    //debug and beta
+    counter = 0;
+	ofSetCircleResolution(50);
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+counter = counter + 0.033f;
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
+    
+    // Gl frame clear so we can capture it with syphon
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    //----------------draw to shyphon here---------------
+
+    
+    //let's draw a circle:
+	ofSetColor(255,130,0);
+	float radius = 50 + 10 * sin(counter);
+	ofFill();		// draw "filled shapes"
+	ofCircle(ofGetWindowSize()/2,radius);
+    
+    
+    
+    mainOutput.publishScreen(); // push the frame to the server
+    
+    
+    //----------------draw outside shyphon here---------------
+    
+    //debug stuff
+    ofDrawBitmapString("hello debug view", 10,10);
 
 }
 
