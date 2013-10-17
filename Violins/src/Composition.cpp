66667ebe06276814ce819b2ee3b07529fa7ff093
Owen Herterich <oherterich@gmail.com>
2013-10-17 13:33:27 -0400

    claimed a track

--- Violins/src/Composition.cpp
+++ Violins/src/Composition.cpp
@@ -61,31 +61,31 @@ void Composition::track2LineCorpseOrder(){
 // Track 3
 
 void Composition::track3Orbit1(){
-    
+    //Owen will work on this, but I welcome help :)
 }
 
 void Composition::track3Corruption1(){
-    
+    //Owen will work on this, but I welcome help :)
 }
 
 void Composition::track3Orbit2(){
-    
+    //Owen will work on this, but I welcome help :)
 }
 
 void Composition::track3Corruption2(){
-    
+    //Owen will work on this, but I welcome help :)
 }
 
 void Composition::track3Orbit3(){
-    
+    //Owen will work on this, but I welcome help :)
 }
 
 void Composition::track3Corruption3(){
-    
+    //Owen will work on this, but I welcome help :)
 }
 
 void Composition::track3FinalBattle(){
-    
+    //Owen will work on this, but I welcome help :)
 }
 
 // Track 4
