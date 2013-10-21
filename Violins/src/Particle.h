e0fa0f0429e4175334ddf37eab94cbd7a2dbdd12
Anthony Marefat <marefatk@gmail.com>
2013-10-21 16:30:57 -0400

    Particle class added

---
+++ Violins/src/Particle.h
@@ -0,0 +1,53 @@
+//
+//  Particle.h
+//  particle0
+//
+//  Created by Keion Anthony Marefat on 10/10/13.
+//
+//
+
+#include "ofMain.h"
+#pragma once
+
+class Particle {
+public:
+    Particle();
+    
+    void update();
+    void draw();
+    
+    void setParams( float px, float py, float vx, float vy, float _size );
+    void addForce( ofVec2f force );
+    void addDampingForce();
+    void addDampingForce(float val);
+    void addRepulsionForce( float px, float py, float radius, float strength);
+    void addAttractionForce( float px, float py, float radius, float strength);
+    void addClockwiseForce( float px, float py, float radius, float strength);
+    void addCounterClockwiseForce( float px, float py, float radius, float strength);
+    void burst(float px, float py, float multiplier);
+    void xenoToPoint(float catchX, float catchY, float catchUpSpeed);
+    void lerpToColor(ofColor start, ofColor end, float amt);
+    void resize(float target, float startTime, float endTime);
+    void addNoise(float vigor);
+    void applyGravity(float g, float floor);
+    void applyBounds();
+    void resetForces();
+    void ageVisuals( bool changeTrans, bool changeSize );
+    bool dead();
+    
+    ofVec2f pos;
+    ofVec2f vel;
+    ofVec2f frc;
+    
+    ofColor c;
+    float transparency;
+    float size;
+    float initSize;
+    float newSize;
+    
+    float age, life;
+    
+    ofVec2f damping;
+    
+    
+};
