e6b116eb9fbc4e6ad1a6ed83d1062462160bf5ea
Anthony Marefat <marefatk@gmail.com>
2013-10-24 17:44:04 -0400

    Can now push keys for circle spawn
    Crashes. Reason unknown.
    
    CPU: 12%
    Memory: 170MB

--- gallifrey/src/testApp.cpp
+++ gallifrey/src/testApp.cpp
@@ -2,14 +2,12 @@
 
 //--------------------------------------------------------------
 void testApp::setup(){
-    ofSetCircleResolution(200);
+    ofSetCircleResolution(1000);
     
     ofBackground(0);
     ofNoFill();
     
-    for (int i = 0; i<25; i++) {
-        cirList[i].set(ofRandomHeight(), ofRandomWidth());
-    }
+    cirList.push_back(ofVec2f(ofGetWidth()/2, ofGetHeight()/2));
 
 }
 
@@ -27,18 +25,31 @@ void testApp::draw(){
     ofTranslate(ofGetWindowSize()/2);
 //  myCircle.drawCircle(0,0,250);
     
-    for (int i = 0; i<25; i++) {
+    for (int i = 0; i<cirList.size(); i++) {
         myMap.Place(0,0,250);
         ofCircle(cirList[i].x, cirList[i].y, myMap.posList[i].z);
-        cirList[i]-=(cirList[i]-myMap.posList[i]).normalize();
+        cirList[i]-=(cirList[i]-myMap.posList[i]).normalized();
     }
     
-    
+    cirList[0]=myMap.posList[0];
     ofPopMatrix();
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
+    
+    float x;
+    float y;
+    
+    float temp = rand()%2;
+    float temp2= rand()%2;
+    
+    if (temp==0)  x = ofRandom(-.3*ofGetWidth(), -.1*ofGetWidth());
+    if (temp==1)  x = ofRandom(1.1*ofGetWidth(), 1.3*ofGetWidth());
+    if (temp2==0)  y = ofRandom(-.3*ofGetHeight(), -.1*ofGetHeight());
+    if (temp2==1)  y = ofRandom(1.1*ofGetHeight(), 1.3*ofGetHeight());
+        
+    cirList.push_back(ofVec2f(x,y));
 
 }
 
